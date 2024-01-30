3e2ae474d61e07bb6dfa254c98c72fd03f57ce21
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-30 19:06:59 -0300

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Main from "@/components/Main";
+
 const Home = () => {
-  return <h1>hello</h1>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
