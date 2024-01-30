7a97556d056682f4971cac51b5510b59c5646108
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-30 19:05:09 -0300

    feat: add main component

---
+++ components/Main/index.tsx
@@ -0,0 +1,13 @@
+import Cards from "../Cards";
+import GetStarted from "../GetStarted";
+
+const Main = () => {
+  return (
+    <main>
+      <Cards />
+      <GetStarted />
+    </main>
+  );
+};
+
+export default Main;
