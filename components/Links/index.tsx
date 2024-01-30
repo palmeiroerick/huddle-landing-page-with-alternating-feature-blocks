7a60c5356c237136d919880be3884197b92ad154
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-30 19:05:54 -0300

    feat: add links component

---
+++ components/Links/index.tsx
@@ -0,0 +1,14 @@
+const Links = () => {
+  return (
+    <section>
+      <button>About Us</button>
+      <button>What We Do</button>
+      <button>FAQ</button>
+      <button>Career</button>
+      <button>Blog</button>
+      <button>Contact Us</button>
+    </section>
+  );
+};
+
+export default Links;
