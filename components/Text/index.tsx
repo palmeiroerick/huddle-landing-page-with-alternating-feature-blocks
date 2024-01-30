63892a7c29bf99b978cbdc934c048eb9a0660d47
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-30 19:02:42 -0300

    feat: add text component

---
+++ components/Text/index.tsx
@@ -0,0 +1,11 @@
+const Text = () => {
+  return (
+    <p>
+      Huddle re-imagines the way we build communities. You have a voice, but so
+      does your audience. Create connections with your users as you engage in
+      genuine discussion.
+    </p>
+  );
+};
+
+export default Text;
