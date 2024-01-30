718d4301d01a64fc661309e447fab12bf2f9fb31
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-30 19:04:52 -0300

    feat: add getstarted component

---
+++ components/GetStarted/index.tsx
@@ -0,0 +1,10 @@
+const GetStarted = () => {
+  return (
+    <div>
+      <h2>Ready To Build Your Community?</h2>
+      <p>Get Started For Free</p>
+    </div>
+  );
+};
+
+export default GetStarted;
