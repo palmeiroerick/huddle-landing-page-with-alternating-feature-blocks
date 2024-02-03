44c093d28482dced9a12b4f401c6af454e2c8d83
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-03 00:14:34 -0300

    style: add styles to mockup component

--- components/Mockups/index.tsx
+++ components/Mockups/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Mockups = () => {
   return (
-    <div className="relative w-[1035px] h-[739px]">
+    <div className="relative m-auto h-[216px] w-[300px]">
       <Image
         className="object-contain"
         src="/assets/illustration-mockups.svg"
