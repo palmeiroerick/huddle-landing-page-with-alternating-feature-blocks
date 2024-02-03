6cff3cca8b137a7bd85e8a30f3d7c743a5a1a7a2
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-03 00:15:35 -0300

    style: add styles to getstarted component

--- components/GetStarted/index.tsx
+++ components/GetStarted/index.tsx
@@ -1,8 +1,12 @@
 const GetStarted = () => {
   return (
-    <div>
-      <h2>Ready To Build Your Community?</h2>
-      <p>Get Started For Free</p>
+    <div className="relative top-28 z-10 flex h-44 flex-col items-center justify-center gap-6 rounded-2xl bg-white shadow-[0_0_12px] shadow-[#00000022]">
+      <h2 className="font-poppins text-lg font-bold">
+        Ready To Build Your Community?
+      </h2>
+      <button className="h-10 w-48 rounded-full bg-pink font-openSans text-xs font-bold text-white shadow-[0_2px_12px] shadow-[#a8c1ffa1]">
+        Get Started For Free
+      </button>
     </div>
   );
 };
