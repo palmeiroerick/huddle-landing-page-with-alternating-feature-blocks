33a2d47f5acb89607414373a17d4eee9bf91e6ff
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-03 00:14:12 -0300

    style: add styles to title component

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -1,5 +1,9 @@
 const Title = () => {
-  return <h1>Build The Community Your Fans Will Love</h1>;
+  return (
+    <h1 className="text-2xl font-poppins font-bold text-center px-3">
+      Build The Community Your Fans Will Love
+    </h1>
+  );
 };
 
 export default Title;
