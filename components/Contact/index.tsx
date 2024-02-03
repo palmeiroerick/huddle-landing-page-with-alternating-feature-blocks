5436c0a7942a6c2ad78f169de65b551d62856ecb
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-03 00:13:29 -0300

    style: add styles to contact component

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -3,20 +3,38 @@ import Image from "next/image";
 const Contact = () => {
   return (
     <>
-      <div>
-        <Image src="/assets/icon-location.svg" alt="" width={13} height={18} />
-        <p>
+      <div className="flex gap-6 pr-1">
+        <Image
+          className="mt-1 h-[18px]"
+          src="/assets/icon-location.svg"
+          alt=""
+          width={13}
+          height={18}
+        />
+        <p className="font-openSans">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua
         </p>
       </div>
-      <div>
-        <Image src="/assets/icon-phone.svg" alt="" width={18} height={18} />
-        <p>+1-543-123-4567</p>
+      <div className="flex gap-[19px]">
+        <Image
+          className="mt-1 h-[18px]"
+          src="/assets/icon-phone.svg"
+          alt=""
+          width={18}
+          height={18}
+        />
+        <p className="font-openSans">+1-543-123-4567</p>
       </div>
-      <div>
-        <Image src="/assets/icon-email.svg" alt="" width={20} height={16} />
-        <p>example@huddle.com</p>
+      <div className="flex gap-[17px]">
+        <Image
+          className="mt-1 h-4"
+          src="/assets/icon-email.svg"
+          alt=""
+          width={20}
+          height={16}
+        />
+        <p className="font-openSans">example@huddle.com</p>
       </div>
     </>
   );
