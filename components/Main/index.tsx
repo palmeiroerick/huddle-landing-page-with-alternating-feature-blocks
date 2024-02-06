254276be4b0329bf1a99c73fe6c6d95381ac0704
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-05 23:31:09 -0300

    style: add desktop styles to main component

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -3,7 +3,7 @@ import GetStarted from "../GetStarted";
 
 const Main = () => {
   return (
-    <main className="w-[375px] m-auto px-4 my-14">
+    <main className="w-[375px] m-auto px-4 my-14 lg:w-[1280px] lg:mt-28 lg:p-0">
       <Cards />
       <GetStarted />
     </main>
