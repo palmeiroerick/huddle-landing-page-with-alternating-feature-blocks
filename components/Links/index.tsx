6cd9a13a4779ad7cd950592da847941fae8b91d3
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-05 23:31:43 -0300

    style: add desktop styles to links component

--- components/Links/index.tsx
+++ components/Links/index.tsx
@@ -1,12 +1,16 @@
 const Links = () => {
   return (
-    <section className="flex flex-col gap-4 text-lg font-openSans">
-      <a>About Us</a>
-      <a>What We Do</a>
-      <a>FAQ</a>
-      <a>Career</a>
-      <a>Blog</a>
-      <a>Contact Us</a>
+    <section className="flex flex-col gap-4 lg:flex-row lg:gap-32 font-openSans text-lg lg:mt-[52px]">
+      <div className="flex flex-col gap-4">
+        <a>About Us</a>
+        <a>What We Do</a>
+        <a>FAQ</a>
+      </div>
+      <div className="flex flex-col gap-4">
+        <a>Career</a>
+        <a>Blog</a>
+        <a>Contact Us</a>
+      </div>
     </section>
   );
 };
