e7d1f62f97e75440a7d8224699888c4c933d03d6
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-05 23:30:29 -0300

    style: add desktop styles to header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,18 +5,18 @@ import Title from "../Title";
 
 const Header = () => {
   return (
-    <header className="m-auto flex w-[375px] flex-col gap-24 bg-veryPaleCyan bg-mobile bg-no-repeat p-5">
-      <section className="flex justify-between">
+    <header className="m-auto flex w-[375px] flex-col gap-24 bg-veryPaleCyan bg-mobile bg-no-repeat p-5 lg:w-auto lg:items-center lg:gap-14 lg:bg-desktop lg:bg-top lg:p-8">
+      <section className="flex justify-between lg:w-[1280px]">
         <Logo />
-        <button className="h-6 w-24 rounded-full bg-white font-openSans text-xs font-bold shadow-[0_8px_16px] shadow-[#d4dae9]">
+        <button className="h-6 w-24 rounded-full bg-white font-openSans text-xs font-bold shadow-[0_8px_16px] shadow-[#d4dae9] lg:h-8 lg:w-32">
           Try It Free
         </button>
       </section>
-      <section className="flex flex-col gap-11">
-        <div className="flex flex-col gap-7">
+      <section className="flex flex-col gap-11 lg:w-[1280px] lg:flex-row lg:items-center">
+        <div className="flex flex-col gap-7 lg:w-[480px]">
           <Title />
           <Text />
-          <button className="m-auto h-10 w-60 rounded-full bg-pink font-openSans text-xs font-bold text-white">
+          <button className="m-auto h-10 w-60 rounded-full bg-pink font-openSans text-xs font-bold text-white lg:m-0">
             Get Started For Free
           </button>
         </div>
