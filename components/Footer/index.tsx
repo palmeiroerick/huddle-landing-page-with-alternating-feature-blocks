c858a16f91f196fe5b9203ae375e149439ee5cb2
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-05 23:31:55 -0300

    style: add desktop styles to footer component

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -6,16 +6,18 @@ import SocialIcons from "../SocialIcons";
 
 const Footer = () => {
   return (
-    <footer className="m-auto flex w-[375px] flex-col gap-12 bg-veryDarkCyan px-4 pt-28 pb-16 text-veryPaleCyan">
-      <section className="flex flex-col gap-8">
-        <Logo white />
-        <Contact />
-      </section>
-      <Links />
-      <section className="flex flex-col gap-6 items-center">
-        <SocialIcons />
-        <Copyright />
-      </section>
+    <footer className="bg-veryDarkCyan">
+      <div className="m-auto flex w-[375px] flex-col gap-12 px-4 lg:px-0 pb-16 pt-28 text-veryPaleCyan lg:w-[1280px] lg:flex-row lg:justify-between">
+        <section className="flex flex-col gap-8 lg:w-96 lg:gap-6">
+          <Logo white />
+          <Contact />
+        </section>
+        <Links />
+        <section className="flex flex-col items-center gap-6 lg:mt-[52px] lg:w-72 lg:items-start lg:justify-between lg:h-52">
+          <SocialIcons />
+          <Copyright />
+        </section>
+      </div>
     </footer>
   );
 };
