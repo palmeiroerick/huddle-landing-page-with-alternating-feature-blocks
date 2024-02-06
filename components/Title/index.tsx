ec373a2f2d29b93fa7fd8d13bb5a4e3f0af29ffe
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-05 23:29:48 -0300

    style: add desktop styles to title component

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -1,6 +1,6 @@
 const Title = () => {
   return (
-    <h1 className="text-2xl font-poppins font-bold text-center px-3">
+    <h1 className="px-3 text-center font-poppins text-2xl font-bold lg:p-0 lg:pr-20 lg:text-start lg:text-3xl">
       Build The Community Your Fans Will Love
     </h1>
   );
