0c1fe75972af3b77edb30b766d798160156daa56
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-05 23:29:57 -0300

    style: add desktop styles to text component

--- components/Text/index.tsx
+++ components/Text/index.tsx
@@ -1,6 +1,6 @@
 const Text = () => {
   return (
-    <p className="px-4 text-center font-openSans">
+    <p className="px-4 text-center font-openSans lg:p-0 lg:text-start">
       Huddle re-imagines the way we build communities. You have a voice, but so
       does your audience. Create connections with your users as you engage in
       genuine discussion.
