c918332da502331a5fd05fe072502c65e595a969
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-05 23:31:02 -0300

    style: add desktop styles to getstarted component

--- components/GetStarted/index.tsx
+++ components/GetStarted/index.tsx
@@ -1,10 +1,10 @@
 const GetStarted = () => {
   return (
-    <div className="relative top-28 z-10 flex h-44 flex-col items-center justify-center gap-6 rounded-2xl bg-white shadow-[0_0_12px] shadow-[#00000022]">
-      <h2 className="font-poppins text-lg font-bold">
+    <div className="relative top-28 z-10 flex h-44 flex-col items-center justify-center gap-6 rounded-2xl bg-white shadow-[0_0_12px] shadow-[#00000022] lg:m-auto lg:h-48 lg:w-[600px] lg:gap-8">
+      <h2 className="font-poppins text-lg font-bold lg:text-2xl">
         Ready To Build Your Community?
       </h2>
-      <button className="h-10 w-48 rounded-full bg-pink font-openSans text-xs font-bold text-white shadow-[0_2px_12px] shadow-[#a8c1ffa1]">
+      <button className="h-10 w-48 rounded-full bg-pink font-openSans text-xs font-bold text-white shadow-[0_2px_12px] shadow-[#a8c1ffa1] lg:h-12 lg:w-72">
         Get Started For Free
       </button>
     </div>
