b6ac84e495895016d9eed7d1f0d9dacd9a8f039d
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-05 23:30:08 -0300

    style: add desktop styles to mockups component

--- components/Mockups/index.tsx
+++ components/Mockups/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Mockups = () => {
   return (
-    <div className="relative m-auto h-[216px] w-[300px]">
+    <div className="relative m-auto h-[216px] w-[300px] lg:w-[680px] lg:h-[560px]">
       <Image
         className="object-contain"
         src="/assets/illustration-mockups.svg"
